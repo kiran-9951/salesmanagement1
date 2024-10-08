aee27033da717afed46be4e982c05b0ea5b3441a
Arun280520 <arunkumara@backflipt.com>
2024-10-08 20:08:07 +0530

    updated

---
+++ controllers/agentController.js
@@ -0,0 +1,46 @@
+const Agents = require("../models/agentModel");
+const { agentValidattionSchema } = require("../validations/agentValidation");
+const Agentsdata = async (req, res) => { 
+    try {
+        const { name, email, phone, company_id } = req.body
+        // console.log(name, email, phone, company_id)
+
+        // if (!name || !email || !phone || !company_id) {
+        //     return res.status(404).json({ message: "provide name email phone companyid" })
+        // }
+        const {value, error} = agentValidattionSchema.validate(req.body, { abortEarly: false })
+        if(error){
+            const formattedErrors = error.details.map(err => ({
+                field: err.context.key,
+                message: err.message
+            }));
+            
+            return res.status(400).send({
+                message: "Validation failed",
+                errors: formattedErrors
+            })
+        }
+        const existedagent = await Agents.findOne({ email:email });
+        
+        if (existedagent) {
+            return res.status(404).json({ message: "agent  already exist" })
+        }
+        const agent = new Agents({
+            name: name,
+            email: email,
+            phone: phone,
+            company_id: company_id
+        })
+
+        await agent.save()
+
+        res.status(201).json({ message: "agent created successfully", status: "success",data: agent })
+
+    }
+    catch (error) {
+        res.status(505).json({ message: "internla server error" }, error)
+
+    }
+}
+
+module.exports = Agentsdata
---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const port = 4040
+const app = express()
+const agentRoute =require("./routes/agentRoute")
+const productRoute =require("./routes/productRoute")
+const leadsRoute =require("./routes/leadsRoute")
+const salesRoute =require("./routes/salesRoute")
+const salesSummaryRoutes =require("./routes/salessummaryRoute")
+
+app.use(express.json())
+app.use("/api",agentRoute)
+app.use("/api",productRoute)
+app.use("/api",leadsRoute)
+app.use("/api",salesRoute)
+app.use("/api",salesSummaryRoutes)
+
+const uri = "mongodb+srv://arvakirank:SrWH7xjSUoXBaeDK@cluster1.lfhhp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1"
+
+mongoose.connect(uri, {dbName:"salesDB"})
+    .then(() => {
+        console.log("mongodb connected")
+    })
+    .catch((error) => {
+        console.log("database not connected properly", error)
+    })
+
+app.listen(port, () => {
+    console.log(`server running at${port}`)
+})
